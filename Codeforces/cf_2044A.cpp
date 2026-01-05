849a9f2e2eb5128bb29a603ca9d12bcf92fd390a
TasnimAhamed <tasnim.ahamed.20@gmail.com>
2026-01-06 00:08:03 +0600

    New Problem Solved on Codeforces

---
+++ Codeforces/cf_2044A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+	int n; cin >> n;
+	cout << n - 1 << "\n";   
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
